b8f117f4d808977dc774ca7e675f3e0ca9d53258
Youssef Elhafyani <yelhafyani@protk.com>
2019-02-01 09:34:37 -0500

    add some file to ignore files

--- index.php
+++ index.php
@@ -10,9 +10,37 @@ spl_autoload_register(function ($class_name) {
     }
 });
 
-set_include_path(__DIR__ . "/application/interfaces");
 spl_autoload_extensions('.php');
 spl_autoload_register();
 
 startup::application_start();
 startup::begin_request();
+
+
+function is_php(string $phpVersion)
+{
+    $version = PHP_VERSION;
+    $version_parts = explode(".", $version);
+    $in_version_parts = explode(".", $phpVersion);
+    for ($i = 0; $i < sizeof($in_version_parts); $i ++) {
+        echo $in_version_parts[$i];
+        echo $version_parts[$i];
+        if(!isset($version_parts[$i])){
+            return true;
+        }
+        
+        if ($version_parts[$i] == $in_version_parts[$i]) {
+            continue;
+        }
+        
+        if ($version_parts[$i] >= $in_version_parts[$i]) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    return false;
+}
+
+var_dump( is_php("7.2"));
